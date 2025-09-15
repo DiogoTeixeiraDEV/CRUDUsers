b93c819ab7cdeb5a1c8cbeffe9b0e16544f2a325
DiogoTeixeiraDEV <diogocominteixeira@gmail.com>
2025-09-15 14:47:48 -0300

    First commit

---
+++ prisma/migrations/20250915170744_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateEnum
+CREATE TYPE "public"."Role" AS ENUM ('USER', 'ADMIN');
+
+-- CreateTable
+CREATE TABLE "public"."User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "role" "public"."Role" NOT NULL DEFAULT 'USER',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "public"."User"("username");
---
+++ src/dto/create-users.dto.ts
@@ -0,0 +1,16 @@
+import { IsEmail, IsString, MinLength, IsOptional } from 'class-validator';
+
+
+export class createUserDto {
+    @IsEmail()
+    email : string;
+
+    @IsOptional()
+    @IsString()
+    username?: string;
+
+    @IsString()
+    @MinLength(6)
+    password: String;
+}
+
